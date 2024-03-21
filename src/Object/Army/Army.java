f481a8bb0e77122750a327751bda0a737528114c
Żaneta <kul.zaneta@gmail.com>
2024-03-21 12:31:44 +0100

    Object

---
+++ src/Object/Army/Army.java
@@ -0,0 +1,74 @@
+package Object.Army;
+
+class Army {
+
+    public static void createArmy() {
+        new Unit("John");
+        new Unit("Tom");
+        new Unit("Jacon");
+        new Unit("Ken");
+        new Unit("Mat");
+
+        new Knight("Bob");
+        new Knight("James");
+        new Knight("Paul");
+
+        new General("Thomas");
+
+        new Doctor("Strange");
+    }
+
+
+    // Don't change the code below
+    static class Unit {
+        static String nameUnit;
+        static int countUnit;
+
+        public Unit(String name) {
+            countUnit++;
+            nameUnit = name;
+
+        }
+    }
+
+    static class Knight {
+        static String nameKnight;
+        static int countKnight;
+
+        public Knight(String name) {
+            countKnight++;
+            nameKnight = name;
+
+        }
+    }
+
+    static class General {
+        static String nameGeneral;
+        static int countGeneral;
+
+        public General(String name) {
+            countGeneral++;
+            nameGeneral = name;
+
+        }
+    }
+
+    static class Doctor {
+        static String nameDoctor;
+        static int countDoctor;
+
+        public Doctor(String name) {
+            countDoctor++;
+            nameDoctor = name;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        createArmy();
+        System.out.println(Unit.countUnit);
+        System.out.println(Knight.countKnight);
+        System.out.println(General.countGeneral);
+        System.out.println(Doctor.countDoctor);
+    }
+}
---
+++ src/Object/Calculating_area_of_a_rectangle_using_class_and_methods/Main.java
@@ -0,0 +1,41 @@
+package Object.Calculating_area_of_a_rectangle_using_class_and_methods;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+    static class Rectangle {
+        private double width;
+
+        private double height;
+
+        public Rectangle(double width, double height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double calculateArea() {
+            return width * height;
+        }
+
+
+        public double getWidth() {
+            return width;
+        }
+
+        public double getHeight() {
+            return height;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double width = scanner.nextDouble();
+        double height = scanner.nextDouble();
+
+        Rectangle rectangle = new Rectangle(width, height);
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println(df.format(rectangle.calculateArea()));
+    }
+}
